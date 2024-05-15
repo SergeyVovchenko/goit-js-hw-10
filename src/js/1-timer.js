ad772cd42ec78845b82ce6a51f8805fff893ad21
SergeyVovchenko <serega.vovchenko@gmail.com>
2024-05-15 12:34:33 +0200

    js-hw-10-start

---
+++ src/js/1-timer.js
@@ -0,0 +1,16 @@
+let dateTime = document.querySelector('#datetime-picker');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+dateTime = new flatpickr(dateTime, options);
