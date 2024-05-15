679d7589d736d5323fb4f8e87b909d2f52465307
SergeyVovchenko <154248935+SergeyVovchenko@users.noreply.github.com>
2024-05-15 12:37:38 +0000

    Deploying to gh-pages from @ SergeyVovchenko/goit-js-hw-10@f9114187a59c979c04cd17fefb98a19fec676729 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{f as t}from"./assets/vendor-77e16229.js";let o=document.querySelector("#datetime-picker");const r={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};t(o,r);
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{f as t}from"./assets/vendor-77e16229.js";const o=document.querySelector("#datetime-picker"),r={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};t(o,r);
 //# sourceMappingURL=commonHelpers.js.map
