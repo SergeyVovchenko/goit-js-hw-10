62900c271be8e3bb29b97d9fc84f57c51833a7ed
SergeyVovchenko <serega.vovchenko@gmail.com>
2024-05-17 17:28:49 +0200

    js-hw-10-finish

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,6 +1,9 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 const dateTime = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('button[data-start]');
 const timerDays = document.querySelector('span[data-days]');
@@ -13,71 +16,80 @@ let userSelectedDate = null;
 btnStart.disabled = true;
 
 function btnHandleClick() {
-  let intervalId = null;
-  btnStart.disabled = true;
-  dateTime.disabled = true;
-  btnStart.classList.remove('btn-is-active');
+    let intervalId = null;
+    btnStart.disabled = true;
+    dateTime.disabled = true;
+    btnStart.classList.remove('btn-is-active');
 
-  intervalId = setInterval(() => {
-    const currentTime = Date.now();
-    const deltaTime = userSelectedDate - currentTime;
-    if (deltaTime < 1000) {
-      btnStart.disabled = false;
-      dateTime.disabled = false;
-      clearInterval(intervalId);
-      timerSeconds.textContent = '00';
-      return;
-    }
-    const time = convertMs(deltaTime);
-    updateTimerFace(time);
-  }, 1000);
+    intervalId = setInterval(() => {
+        const currentTime = Date.now();
+        const deltaTime = userSelectedDate - currentTime;
+        if (deltaTime < 1000) {
+            btnStart.disabled = false;
+            dateTime.disabled = false;
+            clearInterval(intervalId);
+            timerSeconds.textContent = '00';
+            return;
+        }
+        const time = convertMs(deltaTime);
+        updateTimerFace(time);
+    }, 1000);
 }
 
 function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-  const days = padStart(Math.floor(ms / day));
-  const hours = padStart(Math.floor((ms % day) / hour));
-  const minutes = padStart(Math.floor(((ms % day) % hour) / minute));
-  const seconds = padStart(Math.floor((((ms % day) % hour) % minute) / second));
+    const days = padStart(Math.floor(ms / day));
+    const hours = padStart(Math.floor((ms % day) / hour));
+    const minutes = padStart(Math.floor(((ms % day) % hour) / minute));
+    const seconds = padStart(
+        Math.floor((((ms % day) % hour) % minute) / second)
+    );
 
-  console.log({ days, hours, minutes, seconds });
-  return { days, hours, minutes, seconds };
+    console.log({ days, hours, minutes, seconds });
+    return { days, hours, minutes, seconds };
 }
 
 function padStart(value) {
-  return String(value).padStart(2, '0');
+    return String(value).padStart(2, '0');
 }
 
 function updateTimerFace({ days, hours, minutes, seconds }) {
-  timerDays.textContent = `${days}`;
-  timerHours.textContent = `${hours}`;
-  timerMinutes.textContent = `${minutes}`;
-  timerSeconds.textContent = `${seconds}`;
+    timerDays.textContent = `${days}`;
+    timerHours.textContent = `${hours}`;
+    timerMinutes.textContent = `${minutes}`;
+    timerSeconds.textContent = `${seconds}`;
 }
 
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    const dateNow = Date.now();
-    const userDate = selectedDates[0];
-    const userDateMs = userDate.getTime();
-    if (dateNow > userDateMs) {
-      btnStart.disabled = true;
-      btnStart.classList.remove('btn-is-active');
-      return alert('Please choose a date in the future');
-    } else {
-      btnStart.disabled = false;
-      btnStart.classList.add('btn-is-active');
-      userSelectedDate = userDateMs;
-    }
-  },
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        const dateNow = Date.now();
+        const userDate = selectedDates[0];
+        const userDateMs = userDate.getTime();
+        if (dateNow > userDateMs) {
+            btnStart.disabled = true;
+            btnStart.classList.remove('btn-is-active');
+            // return alert('Please choose a date in the future');
+            return iziToast.show({
+                message: `&#9888 Please choose a date in the future`,
+                backgroundColor: '#FFA000',
+                messageColor: '#FFF',
+                messageSize: '16px',
+                position: 'topRight',
+            });
+        } else {
+            btnStart.disabled = false;
+            btnStart.classList.add('btn-is-active');
+            userSelectedDate = userDateMs;
+        }
+    },
 };
 
 flatpickr(dateTime, options);
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,7 +1,63 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-iziToast.show({
-  title: 'Hey',
-  message: 'What would you like to add?',
-});
+const inputDelay = document.querySelector('input[name="delay"]');
+const btnCreate = document.querySelector('button');
+const radioBtn = document.querySelector('fieldset');
+
+inputDelay.addEventListener('input', handleInput);
+btnCreate.addEventListener('click', handleCreate);
+radioBtn.addEventListener('input', handleRadio);
+
+let userDelay = 0;
+let userRadio = '';
+
+function handleInput(event) {
+    userDelay = event.target.value;
+}
+
+function handleRadio(event) {
+    userRadio = event.target.value;
+    console.log(event.target.value);
+}
+
+const queueCreate = [];
+function handleCreate(event) {
+    event.preventDefault();
+    queueCreate.push({ delay: `${userDelay}`, value: `${userRadio}` });
+    console.log(queueCreate);
+    console.log(queueCreate[0].delay);
+
+    const prom = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (queueCreate[0].value === 'fulfilled') {
+                resolve(
+                    `&#10004 Fulfilled promise in ${queueCreate[0].delay}ms`
+                );
+                queueCreate.splice(0, 1);
+            } else if (queueCreate[0].value === 'rejected') {
+                reject(`&#10006 Rejected promise in ${queueCreate[0].delay}ms`);
+                queueCreate.splice(0, 1);
+            }
+        }, queueCreate[0].delay);
+    });
+    prom.then(value => {
+        console.log(value);
+        return iziToast.show({
+            message: `${value}`,
+            backgroundColor: '#59A10D',
+            messageColor: '#FFF',
+            messageSize: '16px',
+            position: 'topRight',
+        });
+    }).catch(error => {
+        console.log(error);
+        return iziToast.show({
+            message: `${error}`,
+            backgroundColor: '#EF4040',
+            messageColor: '#FFF',
+            messageSize: '16px',
+            position: 'topRight',
+        });
+    });
+}
